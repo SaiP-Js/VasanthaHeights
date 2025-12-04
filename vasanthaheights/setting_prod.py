6bfee5d1584899b2ae35121441ce61f2073ee8bb
Niharikarpuram <niharikakarpuram@gmail.com>
2025-12-03 19:54:01 -0800

    updating separate files for test and prod

---
+++ vasanthaheights/setting_prod.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    "accounts",
+    "tenants",
+    "owner",
+    "properties",
+    "payments",
+    "requests",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "vasanthaheights.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "vasanthaheights.context_processors.site_contact",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "vasanthaheights.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # Render persistent disk can override this
+    }
+}
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+CONTACT_PHONE = os.environ.get("CONTACT_PHONE", "")
+CONTACT_EMAIL = os.environ.get("CONTACT_EMAIL", "")
+
+LEASING_NAME = os.environ.get("LEASING_NAME", "")
+LEASING_ADDRESS = os.environ.get("LEASING_ADDRESS", "")
+LEASING_PHONE = os.environ.get("LEASING_PHONE", "")
+LEASING_EMAIL = os.environ.get("LEASING_EMAIL", "")
